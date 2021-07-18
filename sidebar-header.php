7bfd05836af093f824d76f67ecce97a74b4ad4ee
Dea Inesia Sri Utami <39438329+BANZAITESLA@users.noreply.github.com>
2021-07-18 11:33:39 +0700

    Upload template sidebar-header.

---
+++ sidebar-header.php
@@ -0,0 +1,55 @@
+<?php
+    function sidehead($filesidebar) {
+        session_start();
+        include_once("$filesidebar");
+        include_once("functions.php");
+?>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Dashboard Koki</title>
+            <style>
+                body {
+                    background-color: #F5F5F5;
+                }
+                .head {
+                    position: fixed;
+                    display: flex;
+                    margin-top: 40px;
+                    margin-left: 28%;
+                }
+                .nama {
+                    display: flex;
+                    justify-content: right;
+                    align-items: center;
+                    width: 56vw;
+
+                    font-weight: bold;
+                    font-size: 16px;
+                }
+            </style>
+        </head>
+        <body>
+            
+            <?php
+                sidebar();
+            ?>
+            <div class="head">
+                <div class="aturlogo">
+                    <?php
+                        logo();
+                    ?>
+                </div>
+                <div class="nama">    
+                    <?php
+                        echo "Hai, ".$_SESSION['nama_pegawai'];
+                    ?>
+                </div>
+            </div>
+        </body>
+        </html>
+<?php
+    }
+?>
