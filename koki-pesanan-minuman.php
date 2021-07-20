8d53bc8b844385a9bf2227f070e2283938cd42f0
Irsyad Ibrahim <muhammadirsyadibrahim21@gmail.com>
2021-07-20 22:46:38 +0700

    Add files via upload

---
+++ koki-pesanan-minuman.php
@@ -0,0 +1,43 @@
+<?php
+    include_once("sidebar-header.php");
+    sidehead("skoki.php");
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pesanan Minuman</title>
+    <link href="https://fonts.googleapis.com/css2?family=Spectral:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/jquery-ui.css">
+    <style>
+    .list{
+            position: absolute;
+            top: 155px;
+            left: 29%;
+            width: 67vw;
+            height: 35vw;
+        }
+    .judul {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            text-align: center;
+
+            font-weight: bold;
+            font-size: 24px;
+            letter-spacing: 5px;
+    }
+    </style>
+</head>
+<body>    
+    <div class="list">
+        <div class="judul"> <!-- judul page -->
+            PESANAN MINUMAN
+        </div>
+        <!-- ada link ke koki-list-pesanan (belum beres) -->
+    </div>
+</body>
+<script src="js/jquery-3.3.1.min.js"></script>
+<script src="js/jquery-ui.min.js"></script>
+</html>
