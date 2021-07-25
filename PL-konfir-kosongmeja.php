406ce9803bdc294806b9cf86eadb2774e560c1a0
Dea Inesia Sri Utami <deainesia223@gmail.com>
2021-07-25 20:38:19 +0700

    Create Kosongkan Meja

---
+++ PL-konfir-kosongmeja.php
@@ -0,0 +1,8 @@
+<?php
+	include_once("functions.php");
+	$nomeja=$_GET["no_meja"];
+	$db=dbConnect();
+	$sql    = "UPDATE meja_dan_kursi SET id_pelanggan = NULL, status='Tersedia' WHERE no_meja='$nomeja'"; /* hapus data menu */
+	$res	= $db->query($sql);
+	$data	= $res->fetch_all(MYSQLI_ASSOC);
+?>
