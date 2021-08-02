f2ff3362adcb5357b61cf6d1c57d7cb21c28ccb7
Dea Inesia Sri Utami <deainesia223@gmail.com>
2021-08-02 15:03:25 +0700

    Upload Pesanan Minuman Koki

---
+++ KK-detail-pesan.php
@@ -0,0 +1,9 @@
+<?php
+	include_once("functions.php");
+	$idmenu=$_GET["id_menu"];
+    $idpesanan = $_GET["pesan"];
+	$db=dbConnect();
+	$sql			    ="UPDATE detail_pesanan SET `status` = 'Selesai' WHERE id_pesanan = '$idpesanan' AND id_menu = '$idmenu'"; /* hapus data menu */
+	$res			    =$db->query($sql);
+	$data			    =$res->fetch_all(MYSQLI_ASSOC);
+?>
